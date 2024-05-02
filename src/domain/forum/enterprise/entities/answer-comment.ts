bcbb44271101e5b515f8690f34f7530792cdfa93
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-05-02 08:23:25 -0300

    raw: Creating the abstract class 'Comment' that represents the base/parent entity for comment entities and updating the 'AnswerComment' and 'QuestionComment' entities to extend all methods and properties present in the 'Comment' entity

--- src/domain/forum/enterprise/entities/answer-comment.ts
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -1,16 +1,12 @@
+import { Comment, CommentProps } from './comment'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
-import { Entity } from '@/core/entities/entity'
 import { Optional } from '@/core/types/optional'
 
-export interface AnswerCommentProps {
-  authorId: UniqueEntityId
-  answerID: UniqueEntityId
-  content: string
-  createdAt: Date
-  updatedAt?: Date
+export interface AnswerCommentProps extends CommentProps {
+  answerId: UniqueEntityId
 }
 
-export class AnswerComment extends Entity<AnswerCommentProps> {
+export class AnswerComment extends Comment<AnswerCommentProps> {
   static create(
     props: Optional<AnswerCommentProps, 'createdAt'>,
     id?: UniqueEntityId
@@ -26,32 +22,7 @@ export class AnswerComment extends Entity<AnswerCommentProps> {
     return answerComment
   }
 
-  get authorId() {
-    return this.props.authorId
-  }
-
   get answerId() {
-    return this.props.answerID
-  }
-
-  get content() {
-    return this.props.content
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
+    return this.props.answerId
   }
 }
--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,16 +1,12 @@
+import { Comment, CommentProps } from './comment'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
-import { Entity } from '@/core/entities/entity'
 import { Optional } from '@/core/types/optional'
 
-export interface QuestionCommentProps {
-  authorId: UniqueEntityId
-  questionID: UniqueEntityId
-  content: string
-  createdAt: Date
-  updatedAt?: Date
+export interface QuestionCommentProps extends CommentProps {
+  questionId: UniqueEntityId
 }
 
-export class QuestionComment extends Entity<QuestionCommentProps> {
+export class QuestionComment extends Comment<QuestionCommentProps> {
   static create(
     props: Optional<QuestionCommentProps, 'createdAt'>,
     id?: UniqueEntityId
@@ -26,32 +22,7 @@ export class QuestionComment extends Entity<QuestionCommentProps> {
     return questionComment
   }
 
-  get authorId() {
-    return this.props.authorId
-  }
-
   get questionId() {
-    return this.props.questionID
-  }
-
-  get content() {
-    return this.props.content
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
+    return this.props.questionId
   }
 }
