b32d3a2f41975baf6e6592dd3243a128875625a6
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-05-16 09:01:46 -0300

    feat: Creating the domain event class 'QuestionBestAnswerChosenEvent' in the 'forum' subdomain, representing the event triggered when the best answer to a question is chosen. Updating the 'Question' entity by modifying the 'set bestAnswerId' accessor method so that whenever a new answer is chosen as the best answer, a new 'QuestionBestAnswerChosenEvent' event is generated. Updating the 'InMemoryQuestionsRepository' repository class by modifying the 'save' and 'create' methods, so that when a question is saved in the database, the events linked to that particular question are triggered. Lastly, creating and testing the 'Subscriber'/'EventHandler' class for 'OnQuestionBestAnswerChosen' in the 'notification' subdomain, responsible for defining the logic to be executed when a 'QuestionBestAnswerChosenEvent' event is triggered, sending a notification to the creator of the chosen answer

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -5,6 +5,7 @@ import { AggregateRoot } from '@/core/entities/aggregate-root'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { QuestionAttachmentList } from './question-attachment-list'
 import { Optional } from '@/core/types/optional'
+import { QuestionBestAnswerChosenEvent } from '../events/question-best-answer-chosen-event'
 
 export interface QuestionProps {
   authorId: UniqueEntityId
@@ -96,6 +97,17 @@ export class Question extends AggregateRoot<QuestionProps> {
   }
 
   set bestAnswerId(bestAnswerId: UniqueEntityId | undefined) {
+    if (bestAnswerId === undefined) {
+      return
+    }
+
+    if (
+      this.props.bestAnswerId === undefined ||
+      !this.props.bestAnswerId.equals(bestAnswerId)
+    ) {
+      this.addDomainEvent(new QuestionBestAnswerChosenEvent(this, bestAnswerId))
+    }
+
     this.props.bestAnswerId = bestAnswerId
     this.touch()
   }
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
@@ -14,6 +15,8 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     const itemIndex = this.items.findIndex(
       item => item.id.toString() === question.id.toString()
     )
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
 
     this.items[itemIndex] = question
   }
@@ -40,6 +43,8 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async create(question: Question): Promise<void> {
     this.items.push(question)
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 
   async delete(question: Question) {
